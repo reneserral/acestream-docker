60d7a504d380e18714bc60604acf70a3da2b4e4c
Rene <rene.serral@upc.edu>
2024-02-26 01:29:25 +0100

    First try at a docker image

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -e
+
+DIRNAME=$(dirname "$0")
+DOCKER_REPOSITORY=${DOCKER_REPOSITORY-"magnetikonline/acestream-server"}
+
+
+. "$DIRNAME/version"
+
+docker build \
+	--build-arg "ACE_STREAM_VERSION=$ACE_STREAM_VERSION" --no-cache "$DIRNAME"
+#	--tag "$DOCKER_REPOSITORY:$ACE_STREAM_VERSION" \
