0801caa73e4670c708662a3f6dc0cceda4d22b6d
Rene <rene.serral@upc.edu>
2025-08-27 16:53:53 +0200

    CD instead of magic

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -e
 
-DIRNAME=$(dirname "$0")
 DOCKER_REPOSITORY=${DOCKER_REPOSITORY-"magnetikonline/acestream-server"}
 
-docker build --no-cache "$DIRNAME"/acestream
+cd acestream
+
+docker build --no-cache .
