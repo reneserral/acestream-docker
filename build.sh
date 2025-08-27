93ef046b421208731ccec92ff38c2049c34167a2
Rene <rene.serral@upc.edu>
2025-08-27 16:51:40 +0200

    Simplify deployment version management

--- build.sh
+++ build.sh
@@ -3,9 +3,4 @@
 DIRNAME=$(dirname "$0")
 DOCKER_REPOSITORY=${DOCKER_REPOSITORY-"magnetikonline/acestream-server"}
 
-
-. "$DIRNAME/version"
-
-docker build \
-	--build-arg "ACE_STREAM_VERSION=$ACE_STREAM_VERSION" --no-cache "$DIRNAME"/acestream
-#	--tag "$DOCKER_REPOSITORY:$ACE_STREAM_VERSION" \
+docker build --no-cache "$DIRNAME"/acestream
